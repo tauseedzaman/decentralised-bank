8270c3082683acf98f0e66bfbf7a5fb9b3616530
tauseedzaman <tauseedzamanofsorana@gmail.com>
2024-03-23 22:42:07 +0500

    more stuff added

--- Bank.sol
+++ Bank.sol
@@ -18,8 +18,10 @@ contract Bank {
         address indexed reciver,
         uint256 amount
     );
+    event BannedLog(address indexed wallet, bool status);
 
     mapping(address => uint256) public balances;
+    mapping(address => bool) public banned_list;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Unatherized action..");
@@ -29,13 +31,23 @@ contract Bank {
     modifier validateTransaction(address payable _receiver) {
         require(msg.sender != _receiver, "You cannot send to yourself.");
         require(msg.value > 0, "Invalid Amount.");
+
+        // check limits
         require(
             msg.value >= LOWER_LIMIT && msg.value <= UPPER_LIMIT,
             "Amount not within limits."
         );
+
+        // check if current balance is wallet
         require(
             balances[msg.sender] >= msg.value,
             "You don't have enough balance."
+        );
+
+        // check if user is not banned
+        require(
+            (banned_list[msg.sender] && banned_list[msg.sender] == true),
+            "You Are banned from using this service. get in touch to learn more.."
         );
         _;
     }
@@ -44,9 +56,15 @@ contract Bank {
         owner = msg.sender;
     }
 
-    function set_bank_name(
-        string memory _name
-    ) public onlyOwner returns (string memory) {
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    function set_bank_name(string memory _name)
+        public
+        onlyOwner
+        returns (string memory)
+    {
         bank_name = _name;
         return bank_name;
     }
@@ -59,9 +77,29 @@ contract Bank {
         UPPER_LIMIT = _amount;
     }
 
+    function ban_user(address _address) public payable onlyOwner {
+        banned_list[_address] = true;
+        emit BannedLog(_address, true);
+    }
+
+    function unban_user(address _address) public payable onlyOwner {
+        banned_list[_address] = false;
+        emit BannedLog(_address, false);
+    }
+
     function deposit() public payable returns (uint256) {
         require(msg.value > 0, "Invalid Amount.");
-        balances[msg.sender] += msg.value;
+
+        // cut the fee and send it to owner
+        uint256 fee = (msg.value * TRANSACTION_FEE) / 100;
+        address payable contractAddress = payable(address(this));
+
+        // send the amount to this contract
+        contractAddress.transfer(msg.value);
+
+        // cut the fee and update user balance
+        uint256 balance_after_fee = msg.value - fee;
+        balances[msg.sender] += balance_after_fee;
         return balances[msg.sender];
     }
 
@@ -69,9 +107,17 @@ contract Bank {
         return balances[msg.sender];
     }
 
-    function withdraw(
-        address payable _reciver
-    ) public payable validateTransaction(_reciver) returns (uint256) {
+    function get_bank_balance() public view returns (uint256) {
+        address payable contractAddress = payable(address(this));
+        return contractAddress.balance;
+    }
+
+    function withdraw(address payable _reciver)
+        public
+        payable
+        validateTransaction(_reciver)
+        returns (uint256)
+    {
         balances[msg.sender] -= msg.value;
         _reciver.transfer(msg.value);
         balances[_reciver] += msg.value;
